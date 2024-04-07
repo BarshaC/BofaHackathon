a5af449b878dae1553574bf472b139056af38817
KJhenelle <144726886+KJhenelle@users.noreply.github.com>
2024-04-07 15:02:53 -0400

    Update registration.py

--- backend/app/controller/registration.py
+++ backend/app/controller/registration.py
@@ -1,11 +1,12 @@
 import mysql.connector
 from mysql.connector import errorcode
-import json
+
 
 try:
     mydb = mysql.connector.connect(
         user = "root",
-        password = '12345678'
+        password = '12345678',
+        database = 'project_data'
         )
     print(mydb)
 except mysql.connector.connect as err:
@@ -20,7 +21,37 @@ else:
 
 cursor= mydb.cursor
 
-def user_registration(user_name, email, passw):
-    cursor('INSERT INTO user_info (email,passw,user_name) VALUES ('username','email','passw');')
-    return()
-user_registration()
+new_user = ("INSERT INTO user_info "
+               "(email, passw, username) "
+               "VALUES (%s, %s, %s)")
+
+new_post = ("INSERT INTO post_data "
+               "(posttext, userid) "
+               "VALUES (%s, %s)") 
+
+def create_new_user(user_name, email, passw):
+    acc_created=False
+    while acc_created!=True:
+        try:
+            acc_info={
+            'email': email,
+            'user_name':user_name,
+            'passw':passw
+            }
+            acc_created=1
+            
+        except:
+            email=input('enter your email: ')
+            user_name=input('create a username: ')
+            passw=input('enter a password: ')
+    cursor.execute(new_user,acc_info)
+    mydb.commit()
+    mydb.close()
+def create_new_post(posttext):
+    pass
+# def user_registration(user_name, email, passw):
+#     cursor('INSERT INTO user_info (email,passw,user_name) VALUES ('username','email','passw');')
+#     return()
+# user_registration()
+for line in mydb:
+    print(mydb)
